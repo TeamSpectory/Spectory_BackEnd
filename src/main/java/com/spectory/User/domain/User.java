b2ce03bc2c172a3b3a5a2ccd5c88265709dd2dce
dayeondayeon <dayeon2438@gmail.com>
2022-05-12 20:17:59 +0900

    Feat : User Entity, UserJoinRequestDto 생성

--- src/main/java/com/spectory/User/domain/User.java
+++ src/main/java/com/spectory/User/domain/User.java
@@ -1,4 +1,26 @@
 package com.spectory.User.domain;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(length=30, nullable = false)
+    private String id;
+
+    @Column(length=30, nullable = false)
+    private String pw;
+
+    @Column(length=30, nullable = false)
+    private String nickname;
 }
---
+++ src/main/java/com/spectory/User/dto/UserJoinRequestDto.java
@@ -0,0 +1,5 @@
+package com.spectory.User.dto;
+
+public class UserJoinRequestDto {
+
+}
