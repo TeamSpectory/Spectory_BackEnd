3cd0c1015a703d274de9e3d848d0a65588408e51
dayeondayeon <dayeon2438@gmail.com>
2022-05-13 19:24:27 +0900

    Feat : 응답용 Status, Message Class 생성

---
+++ src/main/java/com/spectory/Message.java
@@ -0,0 +1,9 @@
+package com.spectory;
+
+public class Message {
+    public static final String JOIN_SUCCESS = "회원가입 성공";
+    public static final String JOIN_FAIL = "회원가입 실패";
+    public static final String LOGIN_SUCCESS = "로그인 성공";
+    public static final String LOGIN_FAIL = "로그인 실패";
+    public static final String INTERNAL_SERVER_ERR = "서버 에러";
+}
---
+++ src/main/java/com/spectory/Status.java
@@ -0,0 +1,8 @@
+package com.spectory;
+
+public class Status {
+    public static final int OK = 200;
+    public static final int BAD_REQUEST = 400;
+    public static final int NOT_FOUND = 404;
+    public static final int INTERNAL_SERVER_ERR = 500;
+}
