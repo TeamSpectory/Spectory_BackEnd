90bca5f80721e00ba6b180894e118fa3ca74cced
hyeok2Wkd <hyeogi5870@gmail.com>
2022-05-26 14:25:32 +0900

    Fix:@Transactional 추가

--- src/test/java/com/spectory/UserServiceTest.java
+++ src/test/java/com/spectory/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Transactional
 public class UserServiceTest {
 
     @Autowired
